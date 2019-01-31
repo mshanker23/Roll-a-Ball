9d5b75bd07faba279f256f5cb10f9e290af40365
Meghna Shanker <mshanker823@gmail.com>
2019-01-30 22:45:11 -0500

    Added Count Text and Win Text
    Added count text and win text UI elements and edited player script to count how many pick up objects had been encountered and then display win text upon completion.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
+
     private Rigidbody rb;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+
     }
 
 
@@ -29,9 +40,19 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
     }
 
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 
 }
 
